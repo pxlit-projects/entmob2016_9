c9bc48cda2e83b755cf8aad8b5b92c5dcefece3a
MisterDjuke <joryjackmaer@outlook.com>
2016-09-29 14:09:42 +0200

    uwp v0.1
    mvvm architecture init
    bluetooth devices call (untested)

---
+++ EuphoricElephant/EuphoricElephant/ViewModels/SensorTagViewModel.cs
@@ -0,0 +1,31 @@
+﻿using EuphoricElephant.Data;
+using EuphoricElephant.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EuphoricElephant.ViewModels
+{
+    public class SensorTagViewModel : SensorTag
+    {
+        private List<string> devices;
+
+        public List<string> Devices
+        {
+            get { return devices; }
+            set { SetProperty(ref devices, value); }
+        }
+
+        public SensorTagViewModel()
+        {
+            Load();
+        }
+
+        private async void Load()
+        {
+            Devices = await SensorTagService.FindAllTagNames();
+        }
+    }
+}
