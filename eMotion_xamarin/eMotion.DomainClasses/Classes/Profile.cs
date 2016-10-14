aaa3a6356bd322bf8af883fccd56fb2d24a3e32f
Drabzes <reynders_giel@hotmail.com>
2016-10-14 10:33:45 +0200

    xamarin
    Profile classe: getters en setters geplaatst

--- eMotion_xamarin/eMotion.DomainClasses/Classes/Profile.cs
+++ eMotion_xamarin/eMotion.DomainClasses/Classes/Profile.cs
@@ -8,5 +8,10 @@ namespace eMotion.DomainClasses.Classes
 {
     public class Profile
     {
+        public int ProfileId { get; set; }
+        public int UserId { get; set; }
+        public int Commands { get; set; }
+        public int actions { get; set; }
+        public string ProfileName { get; set; }
     }
 }
