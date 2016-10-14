a280ee450ad24aeee0207eee511feb6128112913
Drabzes <reynders_giel@hotmail.com>
2016-10-14 13:14:51 +0200

    EF
    Context diagram aangemaakt en nodige dependancies aangemaakt

--- eMotion_xamarin/eMotion.DomainClasses/Classes/Profile.cs
+++ eMotion_xamarin/eMotion.DomainClasses/Classes/Profile.cs
@@ -11,7 +11,13 @@ namespace eMotion.DomainClasses.Classes
         public int ProfileId { get; set; }
         public int UserId { get; set; }
         public int Commands { get; set; }
-        public int actions { get; set; }
+        public int Actions { get; set; }
         public string ProfileName { get; set; }
+
+
+        public virtual User user { get; set; }
+        public virtual Command command { get; set; }
+        public virtual Action action { get; set; }
+        
     }
 }
---
+++ eMotion_xamarin/eMotion.DomainClasses1/Classes/Command.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eMotion.DomainClasses.Classes
+{
+    [Table("command")]
+    public class Command
+    {
+        [Key]
+        public int CommandId { get; set; }
+        public int Command1 { get; set; }
+        public int Command2 { get; set; }
+        public int Command3 { get; set; }
+        public int Command4 { get; set; }
+        
+    }
+}
