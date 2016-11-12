0059d78da586b8f66865c61bb1b1b656b8c5c7ef
MisterDjuke <joryjackmaer@outlook.com>
2016-11-12 11:11:23 +0100

    FFF really added

---
+++ FlippityFloppityFloop/FlippityFloppityFloop/FlippityFloppityFloop.Droid/obj/Debug/android/src/md5f240eecb79ff4578a8033f8b5d196673/MainActivity.java
@@ -0,0 +1,47 @@
+package md5f240eecb79ff4578a8033f8b5d196673;
+
+
+public class MainActivity
+	extends md5b60ffeb829f638581ab2bb9b1a7f4f3f.FormsApplicationActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("FlippityFloppityFloop.Droid.MainActivity, FlippityFloppityFloop.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("FlippityFloppityFloop.Droid.MainActivity, FlippityFloppityFloop.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
