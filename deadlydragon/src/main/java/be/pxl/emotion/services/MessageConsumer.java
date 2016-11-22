84bde1bb3cdaa6f387ffd953dbb559eb07d9d78c
GielReynders <reynders_giel@hotmail.com>
2016-11-22 16:32:39 +0100

    Logging v2
    Loging toegevoegd aan messageConsomer.java

--- deadlydragon/src/main/java/be/pxl/emotion/services/MessageConsumer.java
+++ deadlydragon/src/main/java/be/pxl/emotion/services/MessageConsumer.java
@@ -1,5 +1,6 @@
 package be.pxl.emotion.services;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
@@ -15,6 +16,8 @@ import javax.jms.TextMessage;
 public class MessageConsumer {
     @Autowired
     private JmsTemplate jmsTemplate;
+    
+    final static Logger logger = Logger.getLogger(MessageConsumer.class);
 
     public JmsTemplate getJmsTemplate() {
         return jmsTemplate;
@@ -27,11 +30,14 @@ public class MessageConsumer {
     @JmsListener(destination = "ErrorQueue")
     public void recieveError() throws JMSException {
         Exception ex = (Exception) getJmsTemplate().receiveAndConvert();
+        logger.error("Sorry, something went wrong",ex);
+        
     }
 
     @JmsListener(destination = "LogQueue")
     public void recieveMessage() throws JMSException {
         TextMessage tm = (TextMessage) getJmsTemplate().receive();
         String log = tm.getText();
+        logger.info(log);
     }
 }
